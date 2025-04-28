f33b24ec9ba102641a6a4dd829fb6573f60668a1
Adam Klein <adamklein.mail@gmail.com>
2025-04-29 01:32:19 +0300

    Trying to fix npx

--- situp.js
+++ situp.js
@@ -8,10 +8,11 @@ const electron = require('electron');
 const mainPath = path.join(__dirname, 'main.js');
 
 // Spawn electron with main.js
-const child = spawn(electron, [mainPath], {
-    stdio: 'inherit'
+const child = spawn(electron, [mainPath], { stdio: 'inherit' });
+
+child.on('error', (err) => {
+  console.error('❌  Failed to launch Electron:', err.message);
+  process.exit(1);
 });
 
-child.on('close', (code) => {
-    process.exit(code);
-}); 
+child.on('close', (code) => process.exit(code));
